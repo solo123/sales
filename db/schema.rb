bdf1cdb731cba40d7a08666bd0992960f466ab5b
Jimmy@cp <jimmy@coolpur.com>
2009-12-09 17:57:32 +0800

    !
    update after change to Sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(:version => 20091206145641) do
   end
 
   create_table "sessions", :force => true do |t|
-    t.string   "session_id", :default => "", :null => false
+    t.string   "session_id", :null => false
     t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
