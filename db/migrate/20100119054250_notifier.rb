5acfe5d2e711760622224e355581765ff79a627b
Jimmy@cp.ubu <jimmy@coolpur.com>
2010-01-21 13:01:25 +0800

    a

--- db/migrate/20100119054250_notifier.rb
+++ db/migrate/20100119054250_notifier.rb
@@ -1,9 +1,10 @@
 class Notifier < ActiveRecord::Migration
   def self.up 
     create_table :notifiers do |t|
-      t.column :type,       :string
+      t.column :ntype,      :string
       t.column :member_id,  :int
       t.column :address,    :string
+      t.column :subject,    :string
       t.column :message,    :string
       t.column :send_date,  :datetime
       t.column :sent_date,  :datetime
